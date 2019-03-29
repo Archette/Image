824b7bb230dcb9fb67b59dca36d2139de7c1fbd0
Rixafy <rixafy@gmail.com>
2019-03-29 22:40:48 +0100

    Add ImageRenderPresenter logic

--- src/Presenter/ImageRenderPresenter.php
+++ src/Presenter/ImageRenderPresenter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rixafy\Image\Presenter;
 
+use Ramsey\Uuid\Uuid;
 use Rixafy\Image\ImageFacade;
 use Rixafy\Image\LocaleImage\LocaleImageFacade;
 
@@ -15,8 +16,26 @@ class ImageRenderPresenter extends \Nette\Application\UI\Presenter
     /** @var LocaleImageFacade @inject */
     public $localeImageFacade;
 
+    /**
+     * @param string $id
+     * @param string $urlName
+     * @param string $renderOptions
+     * @param string|null $languageCode
+     * @throws \Nette\Utils\ImageException
+     * @throws \Rixafy\Image\Exception\ImageNotFoundException
+     * @throws \Rixafy\Image\LocaleImage\Exception\LocaleImageNotFoundException
+     */
     public function actionDefault(string $id, string $urlName, string $renderOptions, string $languageCode = null)
     {
-        //TODO: Render logic, figure out if it is LocaleImage or Image entity
+        $options = json_decode(base64_decode($renderOptions));
+
+        if ($languageCode !== null) {
+            $this->localeImageFacade->render(Uuid::fromString($id), $options->width, $options->height, $options->resizeType);
+
+        } else {
+            $this->imageFacade->render(Uuid::fromString($id), $options->width, $options->height, $options->resizeType);
+        }
+
+        exit;
     }
 }
